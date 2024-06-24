a5653d5ec73a101349602bea9e592f381c4161df
Najma Javed <najma.javed@outlook.com>
2024-06-24 16:42:29 +0200

    solved intro to node.js lab

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
+/* class that maintains a sorted list of numbers in ascending order */
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((num1, num2) => num1-num2);
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
 
-  min() {}
+    if(pos>=this.length) throw new Error("OutOfBounds");
+    return this.items[pos];
+  }
 
-  sum() {}
+  max() {
+    if (this.items.length ===0) throw new Error("EmptySortedList");
+    let maxNum =0;
+    this.items.forEach(num => {
+      if (num > maxNum) maxNum = num;      
+    });
+    return maxNum;
+  }
 
-  avg() {}
+  min() {
+    if (this.items.length ===0) throw new Error("Empty SortedList");
+    return this.items[0] //array is sorted in ascending order already, OR classical approach as in previous iteration
+
+  }
+
+  sum() {
+    if (this.items.length ===0) return 0;
+    return this.items.reduce((acc,num)=> {
+      return acc+num;
+    }, 0);
+  }
+
+  avg() {
+    if (this.items.length ===0) throw new Error("EmptySortedList");
+    return this.sum()/this.length;
+  }
 }
 
 module.exports = SortedList;
